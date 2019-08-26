7394d7cc2a36767d108baa8c5dab8bd2a67ea779
Daniil Borovkov <daniilborowkow@gmail.com>
2019-08-26 11:52:26 +0600

    Printable data on html

--- resources/lang/ru/order.php
+++ resources/lang/ru/order.php
@@ -11,6 +11,8 @@ return [
   'show'                  => 'Заказ #:id',
 
   // print strings
+  'agreement'             => 'Аппарат получен в исправленном состоянии, заявленная неисправность устранена, претензий к внешнему виду, качеству работ не имею.',
+  'apparat_get'           => 'Аппарат получил',
   'workshop'              => 'Мастерская по ремонту электроники',
   'subtitle'              => 'Прием на техническое обслуживание аппарата.',
   'order_data'            => 'Квитанция №:id от :date г.',
@@ -19,4 +21,7 @@ return [
   'phone'                 => 'Тел: <b>:phone</b>',
   'opening_hours'         => 'Часы работы: 9.00 - 18.30',
   'opening_hours_weekend' => 'Сб, Вс: 9.30 - 17.00',
+  'client'                => 'Заказчик',
+  'payments'              => 'Оплата | Сумма',
+  'notices_text'          => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, quibusdam. Unde laudantium aut fugiat facilis quos velit id amet tenetur, maxime sit minus placeat possimus, quasi! Omnis, veniam ea minus!',
 ];
--- resources/views/orders/preview.blade.php
+++ resources/views/orders/preview.blade.php
@@ -9,7 +9,7 @@
   {{-- begin header --}}
   <header class="text-center">
     <h1>{{ __('order.workshop') }}</h1>
-    <h3>{{__('order.subtitle')}}</h3>
+    <h5>{{__('order.subtitle')}}</h5>
     <p>{{ __('order.order_data', [
         'id' => $order->id, 
         'date' => \Date::parse($order->created_at)->format('"d" M Y'),
@@ -19,17 +19,80 @@
 
   {{-- beginn main --}}
   <main class="container-fluid">
-    <row class="justify-between bordered-data">
+    <div class="row justify-content-between bordered-data">
       <div class="col-sm-5 bordered-data-right">
         <p class="mb-0"><b>{{__('order.workshop_owner')}}</b></p>
         <p class="mb-0">{{ __('order.workshop_owner_iin') }}</p>
         <p class="mb-0">{{ $order->workshop->name }}</p>
         <p class="mb-0">{!! __('order.phone', ['phone' => '8961059913']) !!}</p>
         <p class="mb-0">{{ __('order.opening_hours') }}</p>
-        <p class="mb-0">{{ __('order.opening_hours_weekend') }}</p>
+        <p>{{ __('order.opening_hours_weekend') }}</p>
       </div>
-    </row>
+      <div class="col-sm-2 text-right">
+        <p class="mb-0"><b>{{ __('order.client') }}</b></p>
+        <p class="mb-0">{{$order->client->name}}</p>
+        <p class="mb-0">{{$order->client->address}}</p>
+        <p class="mb-0">{{$order->client->phone}}</p>
+      </div>
+    </div>
+    {{-- end credentials --}}
+
+    {{-- data --}}
+    <div class="model-data mt-3">
+      <div>{{ __('order.apparat') }}: {{ $order->type->name }} {{ $order->brand->name }} {!! $order->model_data !!}</div>
+      <div>
+        {{__('nomenclature.problem')}}: {!! $order->problem !!}
+      </div>
+
+      <div>
+        {{__('order.payments')}}: {{$order->client_pay}} | {{$order->price}}
+      </div>
+    </div>
+    {{-- end data --}}
+
+    {{-- small text --}}
+    <p class="mt-3"><small>{{ __('order.notices_text') }}</small></p>
+    {{-- end small text --}}
+
+    {{-- client autograph --}}
+    <div class="client">
+      <b>{{__('order.client')}}</b>: ___________________________
+    </div>
+    {{-- end client autograph --}}
+
+    <div class="aggreement mt-5">
+      <p>{{ __('order.agreement') }}</p>
+      <p>{{__('order.apparat_get')}}: __________________________</p>
+    </div>
+
+    <hr size="3" style="height: 3px; color: black; background-color: black">
   </main>
   {{-- end main --}}
+  
+  {{-- footer --}}
+  <footer>
+    <div class="text-center">
+      <h5>{{__('order.subtitle')}}</h5>
+      <p>{{ __('order.order_data', [
+          'id' => $order->id, 
+          'date' => \Date::parse($order->created_at)->format('"d" M Y'),
+        ]) }}</p>
+    </div>
+
+    <div class="order-data">
+      <p class="mb-0"><b>{{ __('order.client') }}</b></p>
+      <p class="mb-0">{{$order->client->name}} {{$order->client->address}} {{$order->client->phone}}</p>
+
+      <div class="mt-3">{{ __('order.apparat') }}: {{ $order->type->name }} {{ $order->brand->name }} {!! $order->model_data !!}</div>
+      <div>
+        {{__('nomenclature.problem')}}: {!! $order->problem !!}
+      </div>
+
+      <div>
+        {{__('order.payments')}}: {{$order->client_pay}} | {{$order->price}}
+      </div>
+    </div>
+  </footer>
+  {{-- end footer --}}
 </body>
 </html>
